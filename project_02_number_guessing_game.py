20cd99f5a485a075392a63bb632a8dc16c01f2fb
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-02-09 23:09:19 +0600

    Update project_02_number_guessing_game.py

--- project_02_number_guessing_game.py
+++ project_02_number_guessing_game.py
@@ -1,2 +1,12 @@
 # Project 02
 # Number Guessing Game
+
+import random
+
+#r = random.randrange(-5, 11)   # -5 to 10
+#r = random.randrange(11)       # 0 to 10
+#r = random.randint(0, 11)      # 0 to 11
+#print(r)
+
+random_number = random.randrange(-5, 11)
+print(random_number)
