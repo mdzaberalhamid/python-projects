099feb0a751efe09628f4c04b0d4651f44a93ce1
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-02-22 14:16:44 +0600

    Update project_05_simple_word_replacement.py

--- project_05_simple_word_replacement.py
+++ project_05_simple_word_replacement.py
@@ -1,1 +1,14 @@
 #simple_word_replacement_program
+
+def replace_word():
+
+    str = "Hi, I am Zaber, hey hey hey!"
+    print(str)
+    print("----------")
+
+    word_to_replace = input("Enter the word to replace? ")
+    new_word = input("Enter the new word? ")
+    print("----------")
+    print(str.replace(word_to_replace, new_word))
+
+replace_word()
