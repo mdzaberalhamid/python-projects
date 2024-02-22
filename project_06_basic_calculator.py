d73991d5f48471071314a943dce044951e6498d9
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-02-23 00:43:52 +0600

    Create project_06_basic_calculator.py

---
+++ project_06_basic_calculator.py
@@ -0,0 +1,13 @@
+# Project 6
+# Basic Calculator
+# Must have functions: add, sub, mul, div
+
+def add(a, b):
+    answer = a + b
+    print(str(a) + " + " + str(b) + " = " + str(answer))
+
+add(1, 2)
+add(3, 6)
+add(9, 5)
+
+# Continuing...
