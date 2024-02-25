00a3796ada7599d7a0553a4fcfde3d609c82cd94
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-02-25 18:06:15 +0600

    Update project_06_basic_calculator.py

--- project_06_basic_calculator.py
+++ project_06_basic_calculator.py
@@ -4,19 +4,19 @@
 
 def add(a, b):
     answer = a + b
-    print("Result of " + str(a) + " + " + str(b) + " = " + str(answer))
+    print("Result of " + str(a) + " + " + str(b) + " = " + str(answer) + "\n")
 
 def sub(a, b):
     answer = a - b
-    print(str(a) + " - " + str(b) + " = " + str(answer))
+    print("Result of " + str(a) + " - " + str(b) + " = " + str(answer) + "\n")
 
 def mul(a, b):
     answer = a * b
-    print(str(a) + " * " + str(b) + " = " + str(answer))
+    print("Result of " + str(a) + " * " + str(b) + " = " + str(answer) + "\n")
 
 def div(a, b):
     answer = a / b
-    print(str(a) + " / " + str(b) + " = " + str(answer))
+    print("Result of " + str(a) + " / " + str(b) + " = " + str(answer) + "\n")
 
 # test functions
 # add(4, 3)
@@ -24,18 +24,41 @@ def div(a, b):
 # mul(3, 3)
 # div(8, 2)
 
-print("Basic Calculator")
-print("A. Addition")
-print("B. Subtraction")
-print("C. Multiplication")
-print("D. Division")
-print("----------")
-choice = input("Input your choice: ")
+while True:
 
-if choice == "A" or choice == "a":
-    print("Addition")
-    a = int(input("Input first number: "))
-    b = int(input("Input second number: "))
-    add(a, b)
+    # print("")
+    print("Basic Calculator")
+    print("A. Addition")
+    print("B. Subtraction")
+    print("C. Multiplication")
+    print("D. Division")
+    print("E. Exit")
+    print("----------")
+    choice = input("Input your choice: ")
 
-# Continuing...
+    if choice == "A" or choice == "a":
+        print("Addition")
+        a = int(input("Input first number: "))
+        b = int(input("Input second number: "))
+        add(a, b)
+    elif choice == "B" or choice == "b":
+        print("Subtraction")
+        a = int(input("Input first number: "))
+        b = int(input("Input second number: "))
+        sub(a, b)
+    elif choice == "C" or choice == "c":
+        print("Multiplication")
+        a = int(input("Input first number: "))
+        b = int(input("Input second number: "))
+        mul(a, b)
+    elif choice == "D" or choice == "d":
+        print("Division")
+        a = int(input("Input first number: "))
+        b = int(input("input second number: "))
+        div(a, b)
+    elif choice == "E" or choice == "e":
+        print("Program ended.")
+        quit()
+    else:
+        print("Invalid input, program ended.")
+        quit()
