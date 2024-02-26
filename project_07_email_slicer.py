2076216b963fd0dc57a4113cff56bde3e624d2b7
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-02-27 01:02:31 +0600

    Update project_07_email_slicer.py

--- project_07_email_slicer.py
+++ project_07_email_slicer.py
@@ -1,5 +1,24 @@
+# Project 7
+# Email Slicer
+
 def main():
     print("Welcome to the email slicer.")
     print("")
 
-main()
+    email_input = input("Input your email: ").lower()
+
+    username, domain = email_input.split("@")
+    domain, extension = domain.split(".")
+
+    print("Username:", username)
+    print("Domain:", domain)
+    print("Extension:", extension, "\n")
+
+while True:
+    main()
+    x = input("Continue(c) or exit(e) the program? ").lower()
+    if x == "e":
+        print("Program closed successfully.")
+        quit()
+    elif x == "c":
+        continue
