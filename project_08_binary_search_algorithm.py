3a3bc3661a8471b848c8f2078988de3850450af0
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-02-29 22:54:47 +0600

    Update project_08_binary_search_algorithm.py

--- project_08_binary_search_algorithm.py
+++ project_08_binary_search_algorithm.py
@@ -1,6 +1,28 @@
 # Project 8
 # Binary Search Algorithm
 
+def binary_search(list, element):
+    middle = 0
+    start = 0
+    end = len(list)
+    steps = 0
 
+    while start<=end:
+        print("Step", steps, ":", str(list[start:end+1]))
 
-# Continuing...
+        steps = steps + 1
+        middle = (start + end) // 2
+
+        if element == list[middle]:
+            return middle
+        elif element < list[middle]:
+            end = middle - 1
+        elif element > list[middle]:
+            start = middle + 1
+
+    return -1    
+
+my_list = [1,2,3,4,5,6,7,8,9]
+target = 1
+
+binary_search(my_list, target)
