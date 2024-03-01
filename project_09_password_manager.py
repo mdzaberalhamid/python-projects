8b6bb729942edc11b988851d538307b8cacd55e5
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-03-01 22:04:01 +0600

    Update project_09_password_manager.py

--- project_09_password_manager.py
+++ project_09_password_manager.py
@@ -3,4 +3,23 @@
 
 pwd = input("What is the master password? ")
 
+def view():
+    pass
+
+def add():
+    pass
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view/add)? press q to quit(q)? ").lower()
+
+    if mode == "q":
+        break
+    elif mode == "view":
+        pass
+    elif mode == "add":
+        pass
+    else:
+        print("Invalid mode.")
+        continue
+
 # Continuing...
