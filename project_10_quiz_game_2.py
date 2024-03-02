ccd8980e1a605e54028931092ead872f79d29004
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-03-02 23:21:56 +0600

    Create project_10_quiz_game_2.py

---
+++ project_10_quiz_game_2.py
@@ -0,0 +1,19 @@
+# Project 10 
+# Quiz program
+
+quiz = {
+    "question1":{
+        "question":"...",
+        "answer":"..."
+    },
+    "question2":{
+        "question":"...",
+        "answer":"..."
+    },
+    "question3":{
+        "question":"...",
+        "answer":"..."
+    }
+}
+
+# Continuing...
