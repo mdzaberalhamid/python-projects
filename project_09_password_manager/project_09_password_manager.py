8b535dc7c56566f570de398624a9150fd05e4f9d
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-03-05 21:45:06 +0600

    Update project_09_password_manager.py

--- project_09_password_manager/project_09_password_manager.py
+++ project_09_password_manager/project_09_password_manager.py
@@ -1,7 +1,23 @@
 # Project 09 
 # Password Manager
 
+from cryptography.fernet import Fernet
+
+# def write_key():
+#     key = Fernet.generate_key()
+#     with open('key.key', 'wb') as key_file:
+#         key_file.write(key)
+
+def load_key():
+    file = open('key.key', 'rb')
+    key = file.read()
+    file.close()
+    return key
+
 master_pwd = input("What is the master password? ")
+
+key = load_key() + master_pwd.bytes
+fer = Fernet(key)
 
 def view():
     with open('passwords.txt', 'r') as f:
