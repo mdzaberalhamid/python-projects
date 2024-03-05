95b20747d0950e30541e156f9a08febd9f9585ad
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-03-06 00:32:40 +0600

    Update project_09_password_manager.py

--- project_09_password_manager/project_09_password_manager.py
+++ project_09_password_manager/project_09_password_manager.py
@@ -3,10 +3,12 @@
 
 from cryptography.fernet import Fernet
 
-# def write_key():
-#     key = Fernet.generate_key()
-#     with open('key.key', 'wb') as key_file:
-#         key_file.write(key)
+def write_key():
+    key = Fernet.generate_key()
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key)
+
+write_key()
 
 def load_key():
     file = open('key.key', 'rb')
@@ -14,9 +16,10 @@ def load_key():
     file.close()
     return key
 
-master_pwd = input("What is the master password? ")
+# master_pwd = input("What is the master password? ")
 
-key = load_key() + master_pwd.bytes
+# key = load_key() + master_pwd.encode()
+key = load_key()
 fer = Fernet(key)
 
 def view():
@@ -25,14 +28,14 @@ def view():
             # print(line.rstrip())
             data = line.rstrip()
             user, passw = data.split("|")
-            print("User:", user, ", Password:", passw)
+            print("User:", user, "| Password:", fer.decrypt(passw.encode()).decode())
 
 def add():
     name = input("Account name: ")
     pwd = input("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + " | " + pwd + "\n")
+        f.write(name + " | " + fer.encrypt(pwd.encode()).decode() + "\n")
 
 while True:
     mode = input("Would you like to add a new password or view existing ones (view/add)? press q to quit(q)? ").lower()
@@ -46,5 +49,4 @@ while True:
     else:
         print("Invalid mode.")
         continue
-
-# Continuing...
+        
