71425bf5be09478c4dfd7e3e30c00e61c71e6e27
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-03-08 00:18:12 +0600

    Update project_10_quiz_game_2.py

--- project_10_quiz_game_2.py
+++ project_10_quiz_game_2.py
@@ -24,4 +24,19 @@ quiz = {
     }
 }
 
+# print(quiz)
+
+score = 0
+
+for key, value in quiz.items():
+    print(value["question"])
+    answer = input("Answer? ")
+
+    if answer.lower() == value["answer"].lower():
+        print("Correct")
+        score = score + 1
+        print("Your score is " + str(score))
+    else:
+        pass
+
 # Continuing...
