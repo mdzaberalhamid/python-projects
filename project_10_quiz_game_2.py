600d3cd31cc89152f0475ea5580f105b74df62d7
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-03-07 22:42:11 +0600

    Update project_10_quiz_game_2.py

--- project_10_quiz_game_2.py
+++ project_10_quiz_game_2.py
@@ -3,16 +3,24 @@
 
 quiz = {
     "question1":{
-        "question":"...",
-        "answer":"..."
+        "question":"What is the capital city of Bangladesh?",
+        "answer":"Dhaka"
     },
     "question2":{
-        "question":"...",
-        "answer":"..."
+        "question":"What is the capital city of Denmark?",
+        "answer":"Copenhagen"
     },
     "question3":{
-        "question":"...",
-        "answer":"..."
+        "question":"What is the capital city of Japan?",
+        "answer":"Tokyo"
+    },
+    "question4":{
+        "question":"What is the capital city of USA?",
+        "answer":"Washington"
+    },
+    "question5":{
+        "question":"What is the capital city of Russia?",
+        "answer":"Moscow"
     }
 }
 
