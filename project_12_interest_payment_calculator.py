285d05e6145e38ed96594cfe72592a183f568589
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-03-12 22:46:41 +0600

    Create project_12_interest_payment_calculator.py

---
+++ project_12_interest_payment_calculator.py
@@ -0,0 +1,12 @@
+# Project 12 
+# Interest Payment Calculator
+
+# Step 1 - Collect the necessary inputs: principal, apr, years
+# Step 2 - Calculate the monthly payment
+# Step 3 - Show to the user
+
+def main():
+    print("This is a monthly payment loan calculator")
+    print("")
+
+# Continuing...
