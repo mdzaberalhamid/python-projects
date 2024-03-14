d31a308ff07ec8a47a5547389cc5abb3d6de520a
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-03-14 15:28:07 +0600

    Create project_13_random_password_generator.py

---
+++ project_13_random_password_generator.py
@@ -0,0 +1,18 @@
+# Project 13 
+# Random Password Generator
+
+# Step 1 - Ask the user if they want to generate a password or not
+# Step 2 - If yes, ask for password length
+# Step 3 - Generate password
+# Step 4 - Print the password
+# If initial response is no, exit program
+
+import string
+import random
+
+characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+
+def generate_password():
+    pass
+
+# Continuing...
