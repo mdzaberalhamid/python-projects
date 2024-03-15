2910a9e5b887325710c1d624c525f6abcda0dd58
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-03-15 19:21:08 +0600

    Update project_13_random_password_generator.py

--- project_13_random_password_generator.py
+++ project_13_random_password_generator.py
@@ -13,6 +13,21 @@ import random
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
 def generate_password():
-    pass
+    password_length = int(input("How long would you like your password to be? "))
+
+    random.shuffle(characters)
+
+    password = []
+
+    for x in range(password_length):
+        password.append(random.choice(characters))
+
+    random.shuffle(password)
+
+    password = "".join(password)
+
+    print(password)
+
+generate_password()
 
 # Continuing...
