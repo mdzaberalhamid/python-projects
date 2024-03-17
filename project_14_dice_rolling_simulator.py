d256c6736ad8b272b39fa1f9f1b660cc8b44b351
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-03-17 22:24:38 +0600

    Update project_14_dice_rolling_simulator.py

--- project_14_dice_rolling_simulator.py
+++ project_14_dice_rolling_simulator.py
@@ -1,6 +1,17 @@
 # Project 14 
 # Dice Rolling Simulator
 
-print("Dice Rolling Simulator")
+# Step 1 - import random module
+# Step 2 - defining a function to roll the dice
+# Step 3 - create a dictionary that will have the values of the dice
+
+import random
+
+def roll_dice():
+    roll = input("Roll the dice? (Yes/No): ")
+
+    while roll.lower() == "yes":
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
 
 # Continuing...
