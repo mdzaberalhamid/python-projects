11ba0393efb304e700e525f0cb01c528922ebf22
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-03-19 23:08:42 +0600

    Create project_15_site_connectivity_checker.py

---
+++ project_15_site_connectivity_checker.py
@@ -0,0 +1,11 @@
+# Project 15 
+# Site Connectivity Checker
+
+# Step 1 - import urllib
+# Step 2 - use urllib.request
+# Step 3 - write a function that takes a url
+# Step 4 - returns a response
+
+import urllib.request as urllib
+
+# Continuing...
