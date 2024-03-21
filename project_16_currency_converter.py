ab95739a4b9a54cf9fd15167be04b01aac641f35
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-03-22 00:08:12 +0600

    Update project_16_currency_converter.py

--- project_16_currency_converter.py
+++ project_16_currency_converter.py
@@ -3,5 +3,17 @@
 
 def main():
     print("This program converts US dollars to Pound Sterling.")
+    print("")
 
-# Continuing...
+    dollars = eval(input("Enter amount in dollars: "))
+
+    pounds = convert_to_pounds(dollars)
+
+    print("That is", pounds, "Pounds.")
+
+convert_to_pounds = lambda dollars: dollars * 0.82
+
+main()
+
+exit = input("\nPress any key to quit...")
+quit()
