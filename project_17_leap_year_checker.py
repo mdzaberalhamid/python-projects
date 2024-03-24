1866f5e4589603121aa1d4956138e5ffba7056b2
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-03-24 21:15:49 +0600

    Update project_17_leap_year_checker.py

--- project_17_leap_year_checker.py
+++ project_17_leap_year_checker.py
@@ -1,6 +1,22 @@
 # Project 17 
 # Leap Year Checker
 
-print("Leap Year Checker")
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("Leap Year!")
+            else:
+                print("Not Leap Year!")
+        else:
+            print("Leap Year!")
+    else:
+        print("Not Leap Year.")
 
-# Continuing...
+print("Leap Year Checker\n")
+year = int(input("Enter year: "))
+
+is_leap_year(year)
+
+q = input("Press any key to quit...")
+quit()
