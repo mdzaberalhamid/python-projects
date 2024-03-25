c6172cc52e5e1a36e1a1e9df9ef688c5319be7a0
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-03-26 00:31:51 +0600

    Update project_18_word_dictionary.py

--- project_18_word_dictionary.py
+++ project_18_word_dictionary.py
@@ -1,6 +1,22 @@
 # Project 18 
 # Word Dictionary
 
+# Step 1 - create a dictionary that has a key/value pair that represents a word and its meaning
+# Step 2 - collect input from a user, input is a word
+# Step 3 - check if the word is in the dictionary
+# Step 4 - print the definition
+
 print("Word Dictionary")
+
+def main():
+    word_dictionary = {
+        "hi":"a way of greeting",
+        "eyes":"an organ for seeing",
+        "chess":"a popular board game"
+    }
+
+    print(word_dictionary)
+
+main()
 
 # Continuing...
