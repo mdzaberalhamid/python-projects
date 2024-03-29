b4f567db9aeff95ea3f0e2ca04d737ba2f6a9d64
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-03-30 00:25:24 +0600

    Update project_20_web_scraper.py

--- project_20_web_scraper.py
+++ project_20_web_scraper.py
@@ -8,6 +8,20 @@ url = "https://mdzaberalhamid.w3spaces.com/"
 
 r = requests.get(url)
 
-print(r)
+# print(r)
 
-# Continuing...
+soup = BeautifulSoup(r.content, 'lxml')
+
+title = soup.find_all('p', {'class':'portfolio-thumb-title'})
+
+# print(title)
+
+# print(title[0].getText())
+txt = title[0].getText()
+# print(txt)
+
+for t in title:
+    print(t.getText())
+
+z = input("Enter any key to quit...")
+quit()
