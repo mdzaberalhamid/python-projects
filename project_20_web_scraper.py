007242422d67a9d2380fbf1a5bac37367fc6d4bc
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-03-29 21:28:20 +0600

    Create project_20_web_scraper.py

---
+++ project_20_web_scraper.py
@@ -0,0 +1,13 @@
+# Project 20 
+# Web Scraper
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://mdzaberalhamid.w3spaces.com/"
+
+r = requests.get(url)
+
+print(r)
+
+# Continuing...
