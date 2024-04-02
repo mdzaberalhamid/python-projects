d970c855fec66e61d2275656497f2300a5c611f7
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-04-02 21:12:31 +0600

    Update project_21_image_resizer.py

--- project_21_image_resizer.py
+++ project_21_image_resizer.py
@@ -1,6 +1,23 @@
 # Project 21 
 # Image Resizer
 
-print('Image Resizer')
+# Step 1 - install pillow if you haven't 
+# Step 2 - import pillow
+# Step 3 - open up the image we want to resize using python
+# Step 4 - print the current size of that image
+# Step 5 - specify the size we want to change it to
+# Step 6 - save the new resized image
+
+from PIL import Image
+
+print('Image Resizer\n')
+
+image = Image.open('qrimg.png')
+
+print(f"Current image size: {image.size}")
+
+resized_image = image.resize((500, 500))
+
+resized_image.save('qrimg-500.png')
 
 # Continuing...
